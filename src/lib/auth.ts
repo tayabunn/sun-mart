89b8ff163aa614cba787db84c029effbcc358ac3
tayabunn <tayabunnesa@gmail.com>
2026-05-04 21:31:06 +0600

    chore: clean up test files and finalize mongodb connection string

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,8 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
-const db = client.db();
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("sun-mart");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
@@ -11,14 +11,7 @@ export const auth = betterAuth({
     client
   }),
   experimental: { joins: true },
-   //...other options
   emailAndPassword: { 
     enabled: true, 
-  }, 
-  socialProviders: { 
-    github: { 
-      clientId: process.env.GITHUB_CLIENT_ID as string, 
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
-    }, 
-  }, 
+  },
 });
